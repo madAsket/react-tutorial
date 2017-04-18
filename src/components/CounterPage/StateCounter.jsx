f9926011237ca1d67f8cb109b6d89cff88cd32b4
madAsket <madasket@gmail.com>
2017-04-18 21:13:21 +0300

    redux

---
+++ src/components/CounterPage/StateCounter.jsx
@@ -0,0 +1,18 @@
+import Counter from './Counter'
+import React, {Component} from 'react'
+
+class StateCounter extends Component{
+    constructor(){
+        super();
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {value:0};
+    }
+    handleClick(){
+        this.setState({value:this.state.value + 1});
+    }
+    render(){
+        return <Counter value={this.state.value} onClick={this.handleClick} />
+    }
+}
+
+export default StateCounter;
